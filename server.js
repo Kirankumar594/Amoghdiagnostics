9df1b662518c75a859bf06b627811665eb714964
Kiran Kumar <kirankumar594@example.com>
2025-09-10 11:25:34 +0530

    qwerty

--- server.js
+++ server.js
@@ -84,5 +84,5 @@ app.get("*", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5010;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
